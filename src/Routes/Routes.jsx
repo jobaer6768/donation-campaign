d6f1d9c8aae7f1ef5b21ee5f0b793312dc1cf95f
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-09-26 20:57:54 +0600

    added routes

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
+
+
+const Routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/Home',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default Routes;
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-center">Main Layout</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
