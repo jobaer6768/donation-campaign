e143387cde01b19e161b5b5d52c123911e4f5540
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-09-26 21:54:45 +0600

    updated navbar section

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -9,9 +9,18 @@ const Navbar = () => {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1 space-x-4">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/donation">Donation</NavLink>
-                    <NavLink to="/statistics">Statistics</NavLink>
+                    <NavLink to="/" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }
+                    >Home</NavLink>
+                    <NavLink to="/donation" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }
+                    >Donation</NavLink>
+                    <NavLink to="/statistics" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                    }
+                    >Statistics</NavLink>
                 </ul>
             </div>
         </div>
