5ee6c8b7c0c5020c30b825ec7c07976c180ab4e6
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-09-27 21:11:00 +0600

    handled see-more btn in donation page

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -8,6 +8,7 @@ const Donations = () => {
 
     const allCategories = useLoaderData();
     const [categoryDonated, setCategoryDonated] = useState([]);
+    const [isShow, setIsShow] = useState(false);
 
     useEffect(() => {
         const storedCategories = getStoredCategories();
@@ -21,13 +22,24 @@ const Donations = () => {
     }, [])
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {
-                categoryDonated.map(category => <Doantion
-                    key={category.id}
-                    category={category}
-                ></Doantion>)
-            }
+        <div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {
+                    isShow ? categoryDonated.map(category => <Doantion
+                        key={category.id}
+                        category={category}
+                    ></Doantion>)
+                        : categoryDonated.slice(0, 4).map(category => <Doantion
+                            key={category.id}
+                            category={category}
+                        ></Doantion>)
+                }
+            </div>
+
+            {categoryDonated.length > 4 && <button onClick={() => setIsShow(!isShow)} className="p-5 mt-4 rounded-lg bg-green-200 block mx-auto">
+                {isShow ? "See less" : "See more"}
+            </button>}
         </div>
     );
 };
